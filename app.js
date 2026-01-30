5817299eadd7fa85ec62ae3304b435d16861b3ef
Meiiii-intro <miaoming1226@gmail.com>
2026-01-30 12:16:49 +0000

    fix static path and root route

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 const express = require("express");
 const app = express();
+
 const http = require("http");
 const server = http.createServer(app);
+
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+const path = require("path");
+
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static("Public"));
+
+app.use(express.static(path.join(__dirname, "Public")));
+
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "Public", "index.html"));
+});
 
 io.on("connection", (socket) => {
   console.log("a user connected:", socket.id);
 
   socket.on("drawing", (data) => {
-    socket.broadcast.emit("drawing", data); 
+    socket.broadcast.emit("drawing", data);
   });
 
   socket.on("disconnect", () => {
